b944aaf5f7863c5a41e37f4ad109407ef3ad3532
karribalu <karri.balu123456@gmail.com>
2025-01-08 20:43:36 +0530

    add client gRPC connectivity

--- crates/clients/client-1/src/main.rs
+++ crates/clients/client-1/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world from client 1!");
+use hello_world::greeter_client::GreeterClient;
+use hello_world::HelloRequest;
+
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Connect to the server
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    // Create a request
+    let request = tonic::Request::new(HelloRequest {
+        name: "Rustacean".to_string(),
+    });
+
+    // Send the request and receive a response
+    let response = client.say_hello(request).await?;
+
+    println!("Response from server: {:?}", response.into_inner().message);
+
+    Ok(())
 }
